2ee85df08afd77664ca311970d6dd95309df36fa
zenklot <zenklot11@gmail.com>
2021-10-26 20:48:38 +0700

    fix mutex (add '=' in iteration)

--- channel_test.go
+++ channel_test.go
@@ -134,15 +134,15 @@ func TestRaceCond(t *testing.T) {
 func TestMutex(t *testing.T) {
 	x := 0
 	var mutex sync.Mutex = sync.Mutex{}
-	for i := 1; i < 1000; i++ {
+
+	for i := 1; i <= 1000; i++ {
 		go func() {
-			for j := 1; j < 100; j++ {
+			for j := 1; j <= 100; j++ {
 				mutex.Lock()
 				x = x + 1
 				mutex.Unlock()
 			}
 		}()
-
 	}
 
 	time.Sleep(5 * time.Second)
