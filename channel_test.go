6cb25665662b04418d28983f992265148e3c2a92
zenklot <zenklot11@gmail.com>
2021-10-26 19:28:17 +0700

    writing code with select channel

--- channel_test.go
+++ channel_test.go
@@ -86,3 +86,28 @@ func TestRangeChannel(t *testing.T) {
 	}
 	fmt.Println("selesai")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	var counter int8 = 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data Dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data Dari Channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
