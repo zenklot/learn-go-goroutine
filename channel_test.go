56be1a66b78f9b6c57ea28380cb7f33fdff2cba1
zenklot <zenklot11@gmail.com>
2021-10-26 19:42:39 +0700

    writing code testing channel with race conddition

--- channel_test.go
+++ channel_test.go
@@ -113,3 +113,19 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestRaceCond(t *testing.T) {
+	x := 0
+
+	for i := 1; i < 1000; i++ {
+		go func() {
+			for j := 1; j < 100; j++ {
+				x = x + 1
+			}
+		}()
+
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}
