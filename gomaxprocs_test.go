46a7229d3273e680bb6da72ae41f8409cdb9ce13
zenklot <zenklot11@gmail.com>
2021-10-27 09:53:28 +0700

    writing code with gomaxprocs

---
+++ gomaxprocs_test.go
@@ -0,0 +1,32 @@
+package learn_go_goroutine
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGetGomax(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCPU := runtime.NumCPU()
+	fmt.Println("Total CPU", totalCPU)
+
+	runtime.GOMAXPROCS(20) // Change Thread Number
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total Goroutine", totalGoroutine)
+
+	group.Wait()
+}
